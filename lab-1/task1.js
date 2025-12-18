abb9ecad54c5cb11317e888e7bcef2f8ed198526
yaswanth-220114 <n220114@rguktn.ac.in>
2025-12-18 13:26:24 +0530

    Add files via upload

---
+++ lab-1/task1.js
@@ -0,0 +1,11 @@
+// var example
+var name = "surya";
+document.writeln("Hello, welcome to the course " + name+"<br><br>");
+
+// let example
+let age = 18;
+document.writeln("Your age is " + age+"<br><br>");
+
+// const example
+const course = "Data Science";
+document.writeln("You are enrolled in " + course+'<br><br>');
